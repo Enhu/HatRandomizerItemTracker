5239f2a2537965af8364841c1f4ec1577cdf9477
Ezequiel Gonzalez <ee.gonzalez.vazquez@gmail.com>
2024-05-03 03:00:13 -0300

    fix layout

--- renderer.js
+++ renderer.js
@@ -1,16 +1,42 @@
 const rowContainers = [
-    document.getElementById('first-row'),
-    document.getElementById('second-row'),
-    document.getElementById('third-row'),
-    document.getElementById('fourth-row'),
-    document.getElementById('fifth-row'),
-    document.getElementById('sixth-row'),
+    document.getElementById('row-one'),
+    document.getElementById('row-two'),
+    document.getElementById('row-three'),
+    document.getElementById('row-four'),
+    document.getElementById('row-five'),
+    document.getElementById('row-six'),
+    document.getElementById('row-seven'),
+    document.getElementById('row-eight'),
+    document.getElementById('row-nine'),
+    document.getElementById('row-ten'),
 ];
 
-const firstRowImages = [
-    { path: './assets/images/timepiece/timepiece.png', text: "x0", textAlignment: "right", counter: true },
-    { path: './assets/images/yarn/defaultyarn.png', text: "x0", textAlignment: "right", counter: true },
+const rowOneImages = [
+    { path: './assets/images/timepiece/timepiece.png', text: "0", textAlignment: "right", counter: true },
+    { path: './assets/images/yarn/defaultyarn.png', text: "0", textAlignment: "right", counter: true },
     { path: './assets/images/umbrella/umbrella.png' },
+    { path: './assets/images/badges/hookshotbadge.png' },
+    { path: './assets/images/alpine_ziplines/horn.png', text: "Birdhouse", textAlignment: "bottom" },
+];
+
+const rowTwoImages = [
+    { path: './assets/images/snatcher_contracts/contract.png', text: "Toilet", textAlignment: "bottom" },
+    { path: './assets/images/snatcher_contracts/contract.png', text: "MDS", textAlignment: "bottom" },
+    { path: './assets/images/subcon_paintings/yellowpainting.png' },
+    { path: './assets/images/deathwish/deathwish.png', text: "0", textAlignment: "right", counter: true },
+    { path: './assets/images/alpine_ziplines/horn.png', text: "Windmill", textAlignment: "bottom" },
+];
+
+const rowThreeImages = [
+
+    { path: './assets/images/snatcher_contracts/contract.png', text: "Manor", textAlignment: "bottom" },
+    { path: './assets/images/snatcher_contracts/contract.png', text: "Well", textAlignment: "bottom" },
+    { path: './assets/images/subcon_paintings/bluepainting.png' },
+    { path: './assets/images/subcon_paintings/greenpainting.png' },
+    { path: './assets/images/alpine_ziplines/horn.png', text: "Twilight", textAlignment: "bottom" },
+];
+
+const rowFourImages = [
     { path: './assets/images/hats/sprinthat.png' },
     { path: './assets/images/hats/brewerhat.png' },
     { path: './assets/images/hats/dwellermask.png' },
@@ -18,36 +44,23 @@ const firstRowImages = [
     { path: './assets/images/hats/timestophat.png' },
 ];
 
-const secondRowImages = [
-    { path: './assets/images/subcon_paintings/yellowpainting.png' },
-    { path: './assets/images/subcon_paintings/bluepainting.png' },
-    { path: './assets/images/subcon_paintings/greenpainting.png' },
-    { path: './assets/images/snatcher_contracts/contract.png', text: "Toilet", textAlignment: "bottom" },
-    { path: './assets/images/snatcher_contracts/contract.png', text: "MDS", textAlignment: "bottom" },
-    { path: './assets/images/snatcher_contracts/contract.png', text: "Manor", textAlignment: "bottom" },
-    { path: './assets/images/snatcher_contracts/contract.png', text: "Well", textAlignment: "bottom" },
-    { path: './assets/images/alpine_ziplines/horn.png', text: "Birdhouse", textAlignment: "bottom" },
-    { path: './assets/images/alpine_ziplines/horn.png', text: "Windmill", textAlignment: "bottom" },
-    { path: './assets/images/alpine_ziplines/horn.png', text: "Lava Cake", textAlignment: "bottom" },
-    { path: './assets/images/alpine_ziplines/horn.png', text: "Twilight Bell", textAlignment: "bottom" },
-];
-
-const thirdRowImages = [
-    { path: './assets/images/badges/badgepin.png', text: "x0", textAlignment: "right", counter: true },
-    { path: './assets/images/badges/hookshotbadge.png' },
+const rowFiveImages = [
     { path: './assets/images/badges/magnetbadge.png' },
     { path: './assets/images/badges/nobonkbadge.png' },
     { path: './assets/images/badges/scooterbadge.png' },
     { path: './assets/images/badges/fasthatterbadge.png' },
     { path: './assets/images/badges/projectilebadge.png' },
-    { path: './assets/images/badges/1hhbadge.png' },
-    { path: './assets/images/badges/camerabadge.png' },
 ];
 
-const fourthRowImages = [
+const rowSixImages = [
+    { path: './assets/images/badges/1hhbadge.png' },
+    { path: './assets/images/badges/camerabadge.png' },
     { path: './assets/images/badges/hoverbadge.png' },
     { path: './assets/images/badges/compassbadge.png' },
     { path: './assets/images/badges/mumblebadge.png' },
+];
+
+const rowSevenImages = [
     { path: './assets/images/badges/mirrorbadge.png' },
     { path: './assets/images/badges/nostalgiabadge.png' },
     { path: './assets/images/badges/peacefulbadge.png' },
@@ -55,45 +68,55 @@ const fourthRowImages = [
     { path: './assets/images/badges/redtrovrbadge.png' },
 ];
 
-const fifthRowImages = [
+const rowEightImages = [
     { path: './assets/images/relics/burgerpatty.png' },
     { path: './assets/images/relics/burgercushion.png' },
     { path: './assets/images/relics/mountainset.png' },
     { path: './assets/images/relics/train.png' },
+    { path: './assets/images/badges/badgepin.png', text: "0", textAlignment: "right", counter: true },
+];
+
+const rowNineImages = [
     { path: './assets/images/relics/crayonbox.png' },
     { path: './assets/images/relics/redcrayon.png' },
     { path: './assets/images/relics/bluecrayon.png' },
     { path: './assets/images/relics/greencrayon.png' },
-];
-
-const sixthRowImages = [
     { path: './assets/images/relics/necklacebust.png' },
-    { path: './assets/images/relics/necklace.png' },
+
+]
+
+const rowTenImages = [
     { path: './assets/images/relics/cakestand.png' },
     { path: './assets/images/relics/cake.png' },
     { path: './assets/images/relics/cakeslice.png' },
     { path: './assets/images/relics/shortcake.png' },
-];
+    { path: './assets/images/relics/necklace.png' },
+]
+
 
 
 const imageSets = [
-    firstRowImages,
-    secondRowImages,
-    thirdRowImages,
-    fourthRowImages,
-    fifthRowImages,
-    sixthRowImages,
+    rowOneImages,
+    rowTwoImages,
+    rowThreeImages,
+    rowFourImages,
+    rowFiveImages,
+    rowSixImages,
+    rowSevenImages,
+    rowEightImages,
+    rowNineImages,
+    rowTenImages
 ];
 
 function updateTextValue(span, increment) {
-    let currentValue = parseInt(span.innerText.replace("x", ""), 10); // Get the current number
+    let currentValue = parseInt(span.innerText, 10);
     if (increment) {
-        currentValue += 1; // Increment the value
+        currentValue += 1;
     } else {
-        currentValue -= 1; // Decrement the value
-        if (currentValue < 0) currentValue = 0; // Ensure it doesn't go below zero
+        currentValue -= 1;
+        if (currentValue < 0) currentValue = 0;
     }
-    span.innerText = `x${currentValue}`; // Update the text with the new value
+    span.innerText = currentValue;
 }
 
 imageSets.forEach((images, index) => {
@@ -111,7 +134,7 @@ imageSets.forEach((images, index) => {
             img.classList.add('grayscale');
 
             img.addEventListener('click', () => {
-                img.classList.toggle('grayscale'); // Toggle grayscale class
+                img.classList.toggle('grayscale');
             });
         }
 
@@ -123,13 +146,12 @@ imageSets.forEach((images, index) => {
             if (image.counter) {
                 text.className = 'text-bottom-right';
                 img.addEventListener('click', () => {
-                    updateTextValue(text, true); // Increment the text value
+                    updateTextValue(text, true);
                 });
 
-                // Right-click to decrement
                 img.addEventListener('contextmenu', (event) => {
-                    event.preventDefault(); // Prevent default context menu
-                    updateTextValue(text, false); // Decrement the text value
+                    event.preventDefault();
+                    updateTextValue(text, false);
                 });
             }
 
